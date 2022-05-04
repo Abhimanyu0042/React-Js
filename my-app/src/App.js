7bf3d8c73535f88147ec905df9b699fb5fb17b65
Abhimanyu0042 <abhimanyu0042.be21@chitkara.edu.in>
2022-05-04 11:30:22 +0530

    React-Js

---
+++ my-app/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ return (
+    /*<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>*/
+    <>
+    <nav>
+      <li>Home</li>
+      <li>Admission</li>
+      <li>Courses</li>
+      <li>Contact</li>
+    </nav>
+    <div className='hsColor'>
+    <h1>Welcome to Chitkara University</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
