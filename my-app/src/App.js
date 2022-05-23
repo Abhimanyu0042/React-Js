aec096be62d810dfcc82ef7534e9d55824c9c8c3
Abhimanyu0042 <abhimanyu0042.be21@chitkara.edu.in>
2022-05-23 10:47:24 +0530

    React-Js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './Components/Navbar';
 import Components2 from './Components/Components2';
--- my-app/src/Components/Components2.js
+++ my-app/src/Components/Components2.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 export default function Component2() {
-  const [text, setText] = useState("in CU, hello guys the text will be converted on clicking !");
+  const [text, setText] = useState("In CU, hello guys the text will be converted on clicking ! ");
   const handleOnClick = () => {
     //console.log("you clicked on the button");
     let newTxt = text.toUpperCase();
@@ -20,13 +20,33 @@ export default function Component2() {
     let newtext =" ";
     setText(newtext);
   }
-  return (
+  const handleOnSubmit = () => {
+    if (text.length<2){
+      alert("Add the text to continue");
+    } 
+    else{
+      alert("text box is filled")
+    }
+  }
+  const Slice = () => {
+    let newtext = text.slice(7,12);
+    setText(newtext);
+  } 
+  const concat = () => {
+    var text1=" hello"
+    let newtext=text.concat(text1,text);
+    setText(newtext);
+  }
+   return (
     <>
       <div>
         <textarea className="form-control my-3" id="txtbox" rows="10" value={text} onChange={handleOnChange} ></textarea><br></br>
         <button className="btn btn-primary mx-2" onClick={handleOnClick}>Convert</button>
         <button className="btn btn-primary mx-2" onClick={replaceStr}>Replace</button>
         <button className="btn btn-primary mx-2" onClick={handleOnClear}>Clear</button>
+        <button className="btn btn-primary mx-2" onClick={handleOnSubmit}>Submit</button>
+        <button className="btn btn-primary mx-2" onClick={Slice}>Slice</button>
+        <button className="btn btn-primary mx-2" onClick={concat}>Concat</button>
       </div>
       <div className='container my-3'>
         <h2> Summary </h2>
