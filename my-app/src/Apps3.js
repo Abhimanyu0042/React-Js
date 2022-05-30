a56361ce3a57548c1eb0056058377359dd481dfa
Abhimanyu0042 <abhimanyu0042.be21@chitkara.edu.in>
2022-05-30 09:51:54 +0530

    React-Js

---
+++ my-app/src/Apps3.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export default class Apps3 extends Component {
+  constructor() {
+      super(); //
+      this.state = {
+          student: [
+              { name: 'Vikas', marks: 5},
+              { name: 'Rajinder', marks: 7},
+              { name: 'Gagan', marks: 8},
+              { name: 'Shiv', marks: 9},
+              { name: 'Yogesh', marks: 7}
+
+          ]
+      }
+    }
+    increment = (name) => {
+        let ppl = this.state.student.map((s) => {
+            if(s.name === name){
+                s.marks++
+            }
+            return s 
+        })
+        this.setState({student: ppl})
+    }
+    render(){
+        return(
+            <div className="App3">
+                {this.state.student.map((student) =>(
+                    <Students name={student.name}
+                    marks={student.marks}
+                    increment={this.increment}
+                    />
+                ) )}
+            </div>
+        )
+    }
+}
